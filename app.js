a500767343c7f4c78952b548a11ea6e2e70a1edf
CaoHuy26 <huyc197@gmail.com>
2019-07-22 17:19:43 +0700

    #3. Trash function delete

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // For generate ID
 const uniqid = require('uniqid');
-const bodyParser = require('body-parser');  
+const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
@@ -26,11 +26,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/admin', (req, res) => {
-    res.status(200).render('users/admin.ejs');    
+    res.status(200).render('users/admin.ejs');
 });
 
 app.get('/create-item', (req, res) => {
-    res.status(200).render('items/create-item.ejs');    
+    res.status(200).render('items/create-item.ejs');
 });
 
 app.post('/create-item', (req, res) => {
@@ -49,31 +49,70 @@ app.post('/create-item', (req, res) => {
         console.log(newItem);
         const items = JSON.parse(data);
         items.push(newItem);
-        
+
         fs.writeFile('./data.json', JSON.stringify(items), (err) => {
             if (err) {
                 throw err;
             }
             res.status(200).redirect('/');
-        }) 
+        })
     });
 });
 
-app.get('/view-item', (req, res) => {
+app.get('/item', (req, res) => {
     fs.readFile('./data.json', (err, data) => {
         if (err) {
             throw err;
         }
         const items = JSON.parse(data);
-        res.status(200).render('items/view-item', {items: items});
+        res.status(200).render('items/view-all-item', { items: items });
     });
 });
 
-// Xem item bằng id
+app.get('/item/:id', (req, res) => {
+    const q = req.params.id;
+    fs.readFile('./data.json', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const items = JSON.parse(data);
+        for (let item of items) {
+            if (item.id === q) {
+                res.status(200).render('items/view-item', { item: item });
+                return;
+            }
+        }
+        res.status(404).send('Not Found');
+    });
+});
+
+// Must be delete by app.delete
+app.get('/item/delete/:id', (req, res) => {
+    const id = req.params.id;
+    fs.readFile('./data.json', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const items = JSON.parse(data);
+        for (let item of items) {
+            if (item.id === id) {
+                items.splice(items.indexOf(item), 1);
+                fs.writeFile('./data.json', JSON.stringify(items), (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.status(200).send('Deleted')
+                    return;
+                });
+            }
+        }
+    });
+});
+
 
 app.listen(3000, (err) => {
     if (err) {
         throw err;
     }
-   console.log('Sever listening on port 3000....'); 
+    console.log('Sever listening on port 3000....');
 });
